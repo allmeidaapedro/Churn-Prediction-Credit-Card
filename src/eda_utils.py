1749d88138c996f8c117048332469f1288272e9e
Pedro Henrique Almeida Oliveira <pedrohenriquealmeidaoliveira@MacBook-Air-de-Pedro.local>
2024-03-23 12:15:36 -0300

    Update eda visualizations

--- src/eda_utils.py
+++ src/eda_utils.py
@@ -23,31 +23,30 @@ filterwarnings('ignore')
 
 palette=sns.color_palette(['#023047', '#e85d04', '#0077b6', '#ff8200', '#0096c7', '#ff9c33'])
 
-def sns_plots(data, features, histplot=True, countplot=False,     
-              barplot=False, barplot_y=None, boxplot=False, 
-              boxplot_x=None, outliers=False, kde=False, 
-              hue=None, color='#023047', palette=palette):
+def analysis_plots(data, features, histplot=True, barplot=False, mean=None, text_y=0.5,    
+                   outliers=False, boxplot=False, boxplot_x=None, kde=False, hue=None, 
+                   nominal=False, color='#023047', figsize=(24, 12)):
     '''
-    Generate Seaborn plots for visualization.
+    Generate plots for univariate and bivariate analysis.
 
-    This function generates various types of Seaborn plots based on the provided
-    data and features. Supported plot types include histograms, count plots,
-    bar plots, box plots, and more.
+    This function generates histograms, horizontal bar plots 
+    and boxplots based on the provided data and features. 
 
     Args:
         data (DataFrame): The DataFrame containing the data to be visualized.
         features (list): A list of feature names to visualize.
         histplot (bool, optional): Generate histograms. Default is True.
-        countplot (bool, optional): Generate count plots. Default is False.
-        barplot (bool, optional): Generate bar plots. Default is False.
-        barplot_y (str, optional): The name of the feature for the y-axis in bar plots.
-        boxplot (bool, optional): Generate box plots. Default is False.
-        boxplot_x (str, optional): The name of the feature for the x-axis in box plots.
-        outliers (bool, optional): Show outliers in box plots. Default is False.
+        barplot (bool, optional): Generate horizontal bar plots. Default is False.
+        mean (bool, optional): Generate mean bar plots of specified feature instead of proportion bar plots. Default is None.
+        text_y (float, optional): Y coordinate for text on bar plots. Default is 0.5.
+        outliers (bool, optional): Generate boxplots for outliers visualization. Default is False.
+        boxplot (bool, optional): Generate boxplots for categories distributions comparison. Default is False.
+        boxplot_x (str, optional): The feature to which the categories will have their distributions compared. Default is None.
         kde (bool, optional): Plot Kernel Density Estimate in histograms. Default is False.
-        hue (str, optional): The name of the feature to use for color grouping. Default is None.
-        color (str, optional): The color for seaborn plots. Default is None.
-        palete (str, option): The color palette for seaborn plots. Default is a custom palette.
+        hue (str, optional): Hue for histogram and bar plots. Default is None.
+        color (str, optional): The color of the plot. Default is '#023047'.
+        figsize (tuple, optional): The figsize of the plot. Default is (24, 12).
+
     Returns:
         None
 
@@ -57,11 +56,11 @@ def sns_plots(data, features, histplot=True, countplot=False,
     '''
     
     try:
-        # Getting num_features and num_rows and iterating over the sublot dimensions.
+        # Get num_features and num_rows and iterating over the sublot dimensions.
         num_features = len(features)
-        num_rows = num_features // 3 + (num_features % 3 > 0)  
-
-        fig, axes = plt.subplots(num_rows, 3, figsize=(20, 5*num_rows))  
+        num_rows = num_features // 3 + (num_features % 3 > 0) 
+        
+        fig, axes = plt.subplots(num_rows, 3, figsize=figsize)  
 
         for i, feature in enumerate(features):
             row = i // 3  
@@ -69,37 +68,55 @@ def sns_plots(data, features, histplot=True, countplot=False,
 
             ax = axes[row, col] if num_rows > 1 else axes[col] 
             
-            if countplot:
-                # Plotting countplot and adding the counts at the top of each bar.
-                if hue:
-                    sns.countplot(data=data, x=feature, hue=hue, ax=ax, palette=palette)
+            if barplot:
+                if mean:
+                    data_grouped = data.groupby([feature])[[mean]].mean().reset_index()
+                    data_grouped[mean] = round(data_grouped[mean], 2)
+                    ax.barh(y=data_grouped[feature], width=data_grouped[mean], color=color)
+                    for index, value in enumerate(data_grouped[mean]):
+                        # Adjust the text position based on the width of the bars
+                        ax.text(value + text_y, index, f'{value:.1f}', va='center', fontsize=15)
                 else:
-                    sns.countplot(data=data, x=feature, hue=hue, ax=ax, color=color)
-                for container in ax.containers:
-                    ax.bar_label(container)
-
-            elif barplot:
-                # Plotting barplot and adding the averages at the top of each bar.
-                ax = sns.barplot(data=data, x=feature, y=barplot_y, hue=hue, ax=ax, ci=None, color=color)
-                for container in ax.containers:
-                    ax.bar_label(container)
-
+                    if hue:
+                        data_grouped = data.groupby([feature])[[hue]].mean().reset_index().rename(columns={hue: 'pct'})
+                        data_grouped['pct'] *= 100
+                    else:
+                        data_grouped = data.groupby([feature])[[feature]].count().rename(columns={feature: 'count'}).reset_index()
+                        data_grouped['pct'] = data_grouped['count'] / data_grouped['count'].sum() * 100
+        
+                    ax.barh(y=data_grouped[feature], width=data_grouped['pct'], color=color)
+                    
+                    if pd.api.types.is_numeric_dtype(data_grouped[feature]):
+                        ax.invert_yaxis()
+                        
+                    for index, value in enumerate(data_grouped['pct']):
+                        # Adjust the text position based on the width of the bars
+                        ax.text(value + text_y, index, f'{value:.1f}%', va='center', fontsize=15)
+                
+                ax.set_yticks(ticks=range(data_grouped[feature].nunique()), labels=data_grouped[feature].tolist(), fontsize=15)
+                ax.get_xaxis().set_visible(False)
+                ax.spines['top'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                ax.spines['bottom'].set_visible(False)
+                ax.spines['left'].set_visible(False)
+                ax.grid(False)
+        
+            elif outliers:
+                # Plot univariate boxplot.
+                sns.boxplot(data=data, x=feature, ax=ax, color=color)
+            
             elif boxplot:
-                # Plotting multivariate boxplot.
+                # Plot multivariate boxplot.
                 sns.boxplot(data=data, x=boxplot_x, y=feature, showfliers=outliers, ax=ax, palette=palette)
 
-            elif outliers:
-                # Plotting univariate boxplot.
-                sns.boxplot(data=data, x=feature, ax=ax, color=color)
-
             else:
-                # Plotting histplot.
-                sns.histplot(data=data, x=feature, hue=hue, kde=kde, ax=ax, color=color, stat='proportion')
+                # Plot histplot.
+                sns.histplot(data=data, x=feature, kde=kde, ax=ax, color=color, stat='proportion', hue=hue)
 
             ax.set_title(feature)  
             ax.set_xlabel('')  
         
-        # Removing unused axes.
+        # Remove unused axes.
         if num_features < len(axes.flat):
             for j in range(num_features, len(axes.flat)):
                 fig.delaxes(axes.flat[j])
